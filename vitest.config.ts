e8ae6ddab41150e88881f32d421ed2a2bc1236c6
Kent C. Dodds <me+github@kentcdodds.com>
2022-03-23 12:33:21 -0600

    fix vitest ignore

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,5 +11,11 @@ export default defineConfig({
     globals: true,
     environment: "happy-dom",
     setupFiles: ["./test/setup-test-env.ts"],
+    include: ["./app/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    watchIgnore: [
+      ".*\\/node_modules\\/.*",
+      ".*\\/build\\/.*",
+      ".*\\/postgres-data\\/.*",
+    ],
   },
 });
