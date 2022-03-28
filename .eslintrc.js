1add21136d51a35e1b5b18524fb93027ffcc8980
Konstantinos Pittas <konstantinos.pittas+github@gmail.com>
2022-03-28 23:15:38 +0300

    chore: reword comment (#35)

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,9 +8,9 @@ module.exports = {
     "@remix-run/eslint-config/jest",
     "prettier",
   ],
-  // we're using vitest which has a very similar API to jest
-  // (so the linting plugins work nicely), but it we have to explicitly
-  // set the jest version.
+  // We're using vitest which has a very similar API to jest
+  // (so the linting plugins work nicely), but we have to
+  // set the jest version explicitly.
   settings: {
     jest: {
       version: 27,
