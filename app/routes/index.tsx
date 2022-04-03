5e333637de0c8759efaf9b11a1f1cc120e4b8bf8
Jacob Roman <jcbrmn06@gmail.com>
2022-04-03 11:16:16 -0400

    Farm stack heading

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -20,7 +20,7 @@ export default function Index() {
             <div className="lg:pb-18 relative px-4 pt-16 pb-8 sm:px-6 sm:pt-24 sm:pb-14 lg:px-8 lg:pt-32">
               <h1 className="text-center text-6xl font-extrabold tracking-tight sm:text-8xl lg:text-9xl">
                 <span className="block uppercase text-blue-500 drop-shadow-md">
-                  Blues Stack
+                  Farm Stack
                 </span>
               </h1>
               <p className="mx-auto mt-6 max-w-lg text-center text-xl text-white sm:max-w-3xl">
