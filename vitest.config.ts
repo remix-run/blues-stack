fdb22261c3b8a429cdd20437186ebe3f4a799ba6
Jacob Roman <jcbrmn06@gmail.com>
2022-04-03 14:11:19 -0400

    added example env back

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,17 +1,26 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import { defineConfig } from "vite";
+import { configDefults, defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths({ projects: ["tsconfig.json"]})],
   test: {
     globals: true,
     environment: "happy-dom",
     setupFiles: ["./test/setup-test-env.ts"],
     include: ["./app/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    exclude: [
+      "node_modules",
+      "dist",
+      ".idea",
+      ".git",
+      ".cache",
+      "build",
+      "postgres-data"
+    ],
     watchIgnore: [
       ".*\\/node_modules\\/.*",
       ".*\\/build\\/.*",
