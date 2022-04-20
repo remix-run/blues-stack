4b470b96666cacb4340bbcd6bd2e7bca17d9fb25
Michaël De Boey <info@michaeldeboey.be>
2022-04-20 02:34:03 +0200

    feat: remove all TypeScript references when `isTypeScript` is passed as `false` to `remix.init`

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,4 @@
-/**
- * @type {import('@types/eslint').Linter.BaseConfig}
- */
+/** @type {import('@types/eslint').Linter.BaseConfig} */
 module.exports = {
   extends: [
     "@remix-run/eslint-config",
@@ -8,12 +6,16 @@ module.exports = {
     "@remix-run/eslint-config/jest-testing-library",
     "prettier",
   ],
+  env: {
+    "cypress/globals": true,
+  },
+  plugins: ["cypress"],
   // We're using vitest which has a very similar API to jest
   // (so the linting plugins work nicely), but we have to
   // set the jest version explicitly.
   settings: {
     jest: {
-      version: 27,
+      version: 28,
     },
   },
 };
