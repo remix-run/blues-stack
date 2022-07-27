f006c2f7d7cf546496240874b1dbf3f761b7f888
Richard Hpa <richard.m.hpa@gmail.com>
2022-07-27 16:49:21 +1200

    added a visitAndCheck function to make sure that the DOM has been mou… (#104)
    added a visitAndCheck function to make sure that the DOM has been mounted

--- cypress/e2e/smoke.cy.ts
+++ cypress/e2e/smoke.cy.ts
@@ -12,7 +12,7 @@ describe("smoke tests", () => {
     };
     cy.then(() => ({ email: loginForm.email })).as("user");
 
-    cy.visit("/");
+    cy.visitAndCheck("/");
     cy.findByRole("link", { name: /sign up/i }).click();
 
     cy.findByRole("textbox", { name: /email/i }).type(loginForm.email);
@@ -30,7 +30,7 @@ describe("smoke tests", () => {
       body: faker.lorem.sentences(1),
     };
     cy.login();
-    cy.visit("/");
+    cy.visitAndCheck("/");
 
     cy.findByRole("link", { name: /notes/i }).click();
     cy.findByText("No notes yet");
