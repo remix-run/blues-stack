c4888ddb257afb4fc5ff937594e3fec85cf90951
Michaël De Boey <info@michaeldeboey.be>
2024-05-17 17:08:53 +0200

    feat: set swapfile size in `fly.toml` (#241)

--- scripts/migrate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-fallocate -l 512M /swapfile
-chmod 0600 /swapfile
-mkswap /swapfile
-echo 10 >/proc/sys/vm/swappiness
-swapon /swapfile
-echo 1 >/proc/sys/vm/overcommit_memory
-npx prisma migrate deploy
