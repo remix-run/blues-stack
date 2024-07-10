5f6ca0d774d1e5045bfc4bfd519b75242715de05
Michaël De Boey <info@michaeldeboey.be>
2024-07-10 20:29:38 +0200

    chore: update dependencies

--- vitest.config.ts
+++ vitest.config.ts
@@ -12,10 +12,5 @@ export default defineConfig({
     environment: "happy-dom",
     setupFiles: ["./test/setup-test-env.ts"],
     include: ["./app/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-    watchExclude: [
-      ".*\\/node_modules\\/.*",
-      ".*\\/build\\/.*",
-      ".*\\/postgres-data\\/.*",
-    ],
   },
 });
